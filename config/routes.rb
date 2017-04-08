a17b2c2b8b214c4cf252757767d9fed86f7515ad
seedy1 <jobe.seedy@yahoo.com>
2017-04-08 10:07:26 +0000

    tags working. sorta kinda

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'visitor/show'
 
+  get 'tags/:tag', to: 'visitor#index', as: :tag
+
   resources :papers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
